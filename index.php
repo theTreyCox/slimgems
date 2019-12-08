b3a385ae3705ce5bd0fadde14e2401ebd2900c04
treycox <treycox@gmail.com>
2019-12-08 16:37:30 -0500

    art to cover

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     // if access is not successful, do this
     //GITHUB
     // echo '<p><a href="https://github.com/login/oauth/authorize?client_id=61fd6c2fbcbfe4f080b8">Sign in with GitHub</a></p>';
-    echo '<p><a href="https://accounts.spotify.com/authorize?client_id=be5ee3a4215c4df69d8149f599c5134a&response_type=code&redirect_uri=www.illustratemyalbumart.com/slimgemz/signin/callback&scope=user">Sign in to Spotify</a></p>';
+    echo '<p><a href="https://accounts.spotify.com/authorize?client_id=be5ee3a4215c4df69d8149f599c5134a&response_type=code&redirect_uri=https%3A%2F%2Fillustratemyalbumcover.com/slimgemz/signin/callback&scope=user">Sign in to Spotify</a></p>';
   }
 
   // perhaps the preferred way to check is using isset
