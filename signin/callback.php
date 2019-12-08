0a8eca48242bf6473a0194be1b7051bdf9aeee8a
treycox <treycox@gmail.com>
2019-12-08 01:11:12 -0500

    callback

---
+++ signin/callback.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Redirect Callback</title>
+  </head>
+  <body>
+    <?php
+      echo "<h2>Redirected Here! Callback.php</h2>";
+     ?>
+  </body>
+</html>
