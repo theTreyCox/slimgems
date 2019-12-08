9910a2f562ee1e72d6e2727c0779026aa203387a
treycox <treycox@gmail.com>
2019-12-08 01:30:48 -0500

    added semicolons

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+  $accessToken = $_SESSION['my_access_token_accessToken'];
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,20 +13,16 @@
     <title>SLIM GƎMZ</title>
 </head>
 <body>
-  <?php
-    session_start();
-    $accessToken = $_SESSION['my_access_token_accessToken'];
-   ?>
 <h1>SLIM GƎMZ</h1>
 <?php
   echo '<p>Access Token:</p>';
   echo '<p><code> . $accessToken . </code></p>';
   echo '<br />';
   if ($accessToken != "") {
-    echo '<p>Logged in!</p>'
+    echo '<p>Logged in!</p>';
   } else {
     // not logged in
-    echo '<p><a href="https://github.com/login/oauth/authorize?client_id=61fd6c2fbcbfe4f080b8">Sign in with GitHub</a></p>'
+    echo '<p><a href="https://github.com/login/oauth/authorize?client_id=61fd6c2fbcbfe4f080b8">Sign in with GitHub</a></p>';
   }
  ?>
     <script src="site.js"></script>
