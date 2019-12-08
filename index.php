cada1f94bac715006ec4ac2b870f54b264ab26d4
treycox <treycox@gmail.com>
2019-12-08 02:27:14 -0500

    maybe this will work

--- index.php
+++ index.php
@@ -24,6 +24,37 @@
     // not logged in
     echo '<p><a href="https://github.com/login/oauth/authorize?client_id=61fd6c2fbcbfe4f080b8">Sign in with GitHub</a></p>';
   }
+
+  function error($msg) {
+    $response = [];
+    $response['success'] = false;
+    $response['message'] = $msg;
+    return json_encode($response);
+  }
+
+  session_start();
+  $accessToken = $_SESSION['my_access_token_accessToken'];
+
+  if ($accessToken == "") {
+    die(error('Eoor: Invalid access token'));
+  }
+
+  $URL = "https://api.github.com/user";
+  $authHeader = "Authorization: token" . $accessToken;
+  $userAgentHeader = "User-Agent: Demo";
+
+  echo $authHeader . '<br />';
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $URL);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER,array('Accept: application/json', $authHeader, $userAgentHeader));
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  $data = json_decode($response);
+
+
  ?>
     <script src="site.js"></script>
 </body>
