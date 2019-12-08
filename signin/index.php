842f27b82d39e8c8639e3fd272821f37fbc769fe
treycox <treycox@gmail.com>
2019-12-08 02:29:22 -0500

    change pages

--- signin/index.php
+++ signin/index.php
@@ -6,7 +6,34 @@
   </head>
   <body>
     <?php
-      echo "<h2>Redirected Here!</h2>";
+    function error($msg) {
+      $response = [];
+      $response['success'] = false;
+      $response['message'] = $msg;
+      return json_encode($response);
+    }
+
+    session_start();
+    $accessToken = $_SESSION['my_access_token_accessToken'];
+
+    if ($accessToken == "") {
+      die(error('Eoor: Invalid access token'));
+    }
+
+    $URL = "https://api.github.com/user";
+    $authHeader = "Authorization: token" . $accessToken;
+    $userAgentHeader = "User-Agent: Demo";
+
+    echo $authHeader . '<br />';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $URL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,array('Accept: application/json', $authHeader, $userAgentHeader));
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $data = json_decode($response);
      ?>
   </body>
 </html>
