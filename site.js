a270f5f2f523fb32e67e92bec2eaa3aa5679f23e
treycox <treycox@gmail.com>
2019-12-08 19:45:56 -0500

    album covers

--- site.js
+++ site.js
@@ -1,1 +1,12 @@
 console.log('Hello from site.js');
+
+for (i = 1; i < 11; i++) {
+  let albumCover = document.querySelectorAll(`.album${i}`);
+  albumCover.forEach(function(cover) {
+    cover.style.backgroundImage = `url('assets/album${i}.png')`;
+    cover.style.backgroundSize = 'contain';
+    cover.style.backgroundPosition = 'center';
+    cover.style.backgroundRepeat = 'no-repeat';
+    cover.style.backgroundColor = 'transparent';
+  });
+}
