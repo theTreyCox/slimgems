24256f10d6b6a4ee90774be2dc3da39f199e30ea
treycox <treycox@gmail.com>
2019-12-08 21:35:49 -0500

    change = to ==

--- site.js
+++ site.js
@@ -32,7 +32,7 @@ function showAlbumCoverOnAlbumBtnClick(albumBtn, albumCover, i) {
 
 // function for showing and hiding an element via opacity
 function showHideElement(element, i) {
-  if (element.style.opacity = '0' && element.classList.contains(`e${i}`)) {
+  if (element.style.opacity == '0' && element.classList.contains(`e${i}`)) {
     element.style.opacity = '1';
     element.style.pointerEvents = 'all';
   } else {
