c39ed1d88d77b069784cbfb30ec09181c96145df
theTreyCox <treycox@gmail.com>
2019-12-09 16:36:05 -0500

    vh ?

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
   </div>
   <div class="platform-choice-area">
       <div class="apple-music">
-      <iframe src="https://embed.music.apple.com/us/playlist/power-off/pl.fcd3abfac1eb489eb8081362c777696a?app=music&amp;itsct=music_box&amp;itscg=30200" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" allow="autoplay *; encrypted-media *;" style="width: 100%; max-width: 300px; overflow: hidden; border-radius: 10px; background: transparent none repeat scroll 0% 0%;" height="450px" frameborder="0"></iframe>
+      <iframe src="https://embed.music.apple.com/us/playlist/power-off/pl.fcd3abfac1eb489eb8081362c777696a?app=music&amp;itsct=music_box&amp;itscg=30200" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" allow="autoplay *; encrypted-media *;" style="width: 100%; max-width: 300px; overflow: hidden; border-radius: 10px; background: transparent none repeat scroll 0% 0%;" height="100vh" frameborder="0"></iframe>
       </div>
       <div class="spotify"></div>
       <div class="youtube"></div>
