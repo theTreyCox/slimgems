3a1be11b130677b24784242abf3557d657d11439
treycox <treycox@gmail.com>
2019-12-08 20:27:53 -0500

    e

--- index.php
+++ index.php
@@ -18,28 +18,28 @@
       <div class="youtube"></div>
     </div>
     <div class="album-buttons">
-      <div class="album-btn-1 album-btn infinite"><span>Infinite</span></div>
-      <div class="album-btn-2 album-btn sslp"><span>SSLP</span></div>
-      <div class="album-btn-3 album-btn mmlp"><span>MMLP</span></div>
-      <div class="album-btn-4 album-btn tes"><span>TES</span></div>
-      <div class="album-btn-5 album-btn encore"><span>Encore</span></div>
-      <div class="album-btn-6 album-btn relapse"><span>Relapse</span></div>
-      <div class="album-btn-7 album-btn recovery"><span>Recovery</span></div>
-      <div class="album-btn-8 album-btn mmlp2"><span>MMLP2</span></div>
-      <div class="album-btn-9 album-btn revival"><span>Revival</span></div>
-      <div class="album-btn-10 album-btn kamikaze"><span>Kamikaze</span></div>
+      <div class="album-btn-1 album-btn infinite e1"><span>Infinite</span></div>
+      <div class="album-btn-2 album-btn sslp e2"><span>SSLP</span></div>
+      <div class="album-btn-3 album-btn mmlp e3"><span>MMLP</span></div>
+      <div class="album-btn-4 album-btn tes e4"><span>TES</span></div>
+      <div class="album-btn-5 album-btn encore e5"><span>Encore</span></div>
+      <div class="album-btn-6 album-btn relapse e6"><span>Relapse</span></div>
+      <div class="album-btn-7 album-btn recovery e7"><span>Recovery</span></div>
+      <div class="album-btn-8 album-btn mmlp2 e8"><span>MMLP2</span></div>
+      <div class="album-btn-9 album-btn revival e9"><span>Revival</span></div>
+      <div class="album-btn-10 album-btn kamikaze e10"><span>Kamikaze</span></div>
     </div>
     <div class="albums-container">
-      <div class="album1 album infinite"></div>
-      <div class="album2 album sslp"></div>
-      <div class="album3 album mmlp"></div>
-      <div class="album4 album tes"></div>
-      <div class="album5 album encore"></div>
-      <div class="album6 album relpase"></div>
-      <div class="album7 album recovery"></div>
-      <div class="album8 album mmlp2"></div>
-      <div class="album9 album revival"></div>
-      <div class="album10 album kamikaze"></div>
+      <div class="album1 album infinite e1"></div>
+      <div class="album2 album sslp e2"></div>
+      <div class="album3 album mmlp e3"></div>
+      <div class="album4 album tes e4"></div>
+      <div class="album5 album encore e5"></div>
+      <div class="album6 album relpase e6"></div>
+      <div class="album7 album recovery e7"></div>
+      <div class="album8 album mmlp2 e8"></div>
+      <div class="album9 album revival e9"></div>
+      <div class="album10 album kamikaze e10"></div>
     </div>
   </div>
 </div>
