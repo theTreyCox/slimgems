c887cc966d3840d7fb377e207d007fb3d5bdda3a
theTreyCox <treycox@gmail.com>
2019-12-09 15:55:08 -0500

    functions

--- site.js
+++ site.js
@@ -1,26 +1,30 @@
 console.log('Hello from site.js');
 
-for (i = 1; i < 11; i++) {
-    // set album cover art for each album
-    let albumCover = document.querySelectorAll(`.album${i}`);
-    albumCover.forEach(function(cover) {
-        // album cover size and position
-        cover.style.backgroundImage = `url('assets/album${i}.png')`;
-        cover.style.backgroundSize = 'contain';
-        cover.style.backgroundPosition = 'center';
-        cover.style.backgroundRepeat = 'no-repeat';
-        cover.style.backgroundColor = 'transparent';
-        // make album art transparent by default;
-        cover.style.opacity = '0';
-        cover.style.pointerEvents = 'none';
-        // run show album cover on button click function
-        let albumBtn = document.querySelectorAll(`.album-btn-${i}`);
-        showAlbumCoverOnAlbumBtnClick(albumBtn, cover, i);
-    });
-}
-
+generateCovers();
 addCDSpinesToButtons();
 flipAllAlbumCovers();
+closeAlbum();
+
+function generateCovers() {
+    for (i = 1; i < 11; i++) {
+        // set album cover art for each album
+        let albumCover = document.querySelectorAll(`.album${i}`);
+        albumCover.forEach(function(cover) {
+            // album cover size and position
+            cover.style.backgroundImage = `url('assets/album${i}.png')`;
+            cover.style.backgroundSize = 'contain';
+            cover.style.backgroundPosition = 'center';
+            cover.style.backgroundRepeat = 'no-repeat';
+            cover.style.backgroundColor = 'transparent';
+            // make album art transparent by default;
+            cover.style.opacity = '0';
+            cover.style.pointerEvents = 'none';
+            // run show album cover on button click function
+            let albumBtn = document.querySelectorAll(`.album-btn-${i}`);
+            showAlbumCoverOnAlbumBtnClick(albumBtn, cover, i);
+        });
+    }
+}
 
 function flipAllAlbumCovers() {
     for (i = 1; i < 11; i++) {
