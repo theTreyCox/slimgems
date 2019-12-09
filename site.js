899dfbf95828f68525c0190bdc7f0e691474916e
treycox <treycox@gmail.com>
2019-12-08 22:12:12 -0500

    test

--- site.js
+++ site.js
@@ -29,9 +29,9 @@ function showAlbumCoverOnAlbumBtnClick(albumBtn, albumCover, i) {
           albumCovClasses.forEach(function(cl) {
             cl.style.opacity = '0';
             cl.style.pointerEvents = 'none';
+            showHideElement(cl, i);
           })
         }
-        showHideElement(albumCover, i);
       }
     });
   });
@@ -40,7 +40,6 @@ function showAlbumCoverOnAlbumBtnClick(albumBtn, albumCover, i) {
 // function for showing and hiding an element via opacity
 function showHideElement(element, i) {
   if (element.style.opacity == '0' && element.classList.contains(`e${i}`)) {
-
     element.style.opacity = '1';
     element.style.pointerEvents = 'all';
     console.log('style opacity = 1 now and class is e' + i);
