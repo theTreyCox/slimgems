f8eb1df849c422e16c8796d11b5fe0a97e0a5df8
treycox <treycox@gmail.com>
2019-12-15 19:07:59 -0500

    remove height

--- site.js
+++ site.js
@@ -243,15 +243,15 @@ function makeResizableDiv(div) {
                 if (width > minimum_size) {
                     element.style.width = width + 'px'
                 }
-                if (height > minimum_size) {
-                    element.style.height = height + 'px'
-                }
+                // if (height > minimum_size) {
+                //     element.style.height = height + 'px'
+                // }
             } else if (currentResizer.classList.contains('bottom-left')) {
                 const height = original_height + (e.pageY - original_mouse_y)
                 const width = original_width - (e.pageX - original_mouse_x)
-                if (height > minimum_size) {
-                    element.style.height = height + 'px'
-                }
+                // if (height > minimum_size) {
+                //     element.style.height = height + 'px'
+                // }
                 if (width > minimum_size) {
                     element.style.width = width + 'px'
                     element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
@@ -262,10 +262,10 @@ function makeResizableDiv(div) {
                 if (width > minimum_size) {
                     element.style.width = width + 'px'
                 }
-                if (height > minimum_size) {
-                    element.style.height = height + 'px'
-                    element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
-                }
+                // if (height > minimum_size) {
+                //     element.style.height = height + 'px'
+                //     element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+                // }
             } else {
                 const width = original_width - (e.pageX - original_mouse_x)
                 const height = original_height - (e.pageY - original_mouse_y)
